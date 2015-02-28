59b097edc7eacf873c1f214ff62fb534929f392d
Karthik <karthik_m_a_m@outlook.com>
2015-02-28 16:38:07 +0530

    Source Files

---
+++ CodeConverters/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace CodeConverters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream inputFile = new FileStream("karthik.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            FileStream outputFile = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamReader reader = new StreamReader(inputFile);
+            StreamWriter writer = new StreamWriter(outputFile);
+            string data;
+            while((data = reader.ReadLine()) != null)
+            {
+                while(Char.IsDigit(data[0]) || data[0] == ' ' || data[0] == '.')
+                {
+                    data = data.Remove(0,1);
+                }
+                writer.WriteLine(data);
+            }
+            writer.Close();
+            reader.Close();
+            inputFile.Close();
+            outputFile.Close();
+        }
+    }
+}
